27ed5474a49763d2d5bfc3f0fc100ec3182b9e6b
Juan Sapién <juansapienflores@gmail.com>
2023-10-15 21:13:14 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_memorama
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_memorama/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_memorama
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
